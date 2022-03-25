545b08e37ab5d6c29fa44ead7a8b1cdc610e1364
deso92 <adjantok@gmail.com>
2022-03-25 12:57:41 +0000

    Affichage de la date en php

--- formation_php.php
+++ formation_php.php
@@ -19,7 +19,7 @@
         <li style="color: green;">Texte en vert</li>
         </ul>
 
-        <?php echo "Ceci est du texte"; ?>
-        
+         <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+
     </body>
 </html>
